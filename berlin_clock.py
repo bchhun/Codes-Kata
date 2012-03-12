3d26abad3ad92c511f012ff0c5fae3cf8112f3da
Bernard Chhun <bchhun@sogix.com>
2012-03-12 11:40:29 -0400

    Refactoring + solution pour les minutes !

--- berlin_clock.py
+++ berlin_clock.py
@@ -9,11 +9,12 @@ class BerlinClockTests(unittest.TestCase):
 
     def test_nightime(self):
         self.assertEqual(berlin_clock("23:59:59"), "O\nRRRR\nRRRO\nYYRYYRYYRYY\nYYYY")
+        self.assertEqual(berlin_clock("23:58:59"), "O\nRRRR\nRRRO\nYYRYYRYYRYY\nYYYO")
     
     def test_midnight_24h(self):
         self.assertEqual(berlin_clock("24:00:00"), "Y\nRRRR\nRRRR\nOOOOOOOOOOO\nOOOO")    
 
-class BerlinClockOutput:
+class BerlinClock:
     y_flag = "Y"
     r_flag = "R"
     off_flag = "O"
@@ -35,14 +36,29 @@ class BerlinClockOutput:
 
         first_row = (int(self.hours) / 5) * self.r_flag
         first_row = self.filler_output(first_row, hours_max_char)
+
         second_row = (int(self.hours) % 5) * self.r_flag
         second_row = self.filler_output(second_row, hours_max_char)
 
         return "%s\n%s" % (first_row, second_row)
 
     def minutes_output(self):
-        first_row = "OOOOOOOOOOO"
-        second_row = "OOOO"
+        first_row_max_char = 11
+        second_row_max_char = 4
+        first_row_single_value = 5
+
+        first_row = "".join(
+            map(
+                lambda x: self.y_flag if x % 15 != 0 else self.r_flag 
+                , range(5, int(self.minutes), 5)
+            )
+        )
+        first_row = self.filler_output(first_row, first_row_max_char)
+
+
+        second_row = self.y_flag * (int(self.minutes) % 5)
+        second_row = self.filler_output(second_row, second_row_max_char)
+
         return "%s\n%s" % (first_row, second_row)
 
     def output(self):
@@ -53,7 +69,7 @@ class BerlinClockOutput:
         ])
 
 def berlin_clock(timestamp):
-    bco = BerlinClockOutput(timestamp)
+    bco = BerlinClock(timestamp)
     return bco.output()
 
 if __name__ == '__main__':
