4f8af014b8fd9c48ef25b9250fdb1a59b14a4eba
Bernard Chhun <bernard.chhun@gmail.com>
2012-10-13 15:09:57 -0400

    Création d'un cercle possible mais seulement avec une color en filling

--- mandala.py
+++ mandala.py
@@ -11,19 +11,25 @@
 from Tkinter import *
 
 class DerivedCanvas(Canvas):
-    def create_circle(self):
+    """
+        Héritage de Canvas pour me permettre de dessiner des cercles
+        avec des paramètres simples
+    """
+    def create_circle(self, x, y, radius, fill = None, outline = None):
         """
             2.2 Start drawing
-        """
 
-        print "create_circle"
+        """
+        return self.create_oval(x - radius, y - radius, x + radius, y + radius, width=0, fill=fill, outline=outline)
 
 def main():
     master = Tk()
 
     w = DerivedCanvas(master, width=400, height=400)
     w.pack()
-    w.create_circle()
+    
+    text = w.create_text(50,10, text="tk test")
+    circle = w.create_circle(100, 100, 20, None, "black")
 
     mainloop()
 
