9a77ad0b32da55548d3e332bcc9f2425ffb1bcc8
Bernard Chhun <bernard.chhun@gmail.com>
2012-10-13 15:16:20 -0400

    Création de cercle avec bordure et filling de la couleur désiré

--- mandala.py
+++ mandala.py
@@ -15,21 +15,19 @@ class DerivedCanvas(Canvas):
         Héritage de Canvas pour me permettre de dessiner des cercles
         avec des paramètres simples
     """
-    def create_circle(self, x, y, radius, fill = None, outline = None):
+    def create_circle(self, x, y, radius, fill = None, outline = None, width = 0):
         """
             2.2 Start drawing
 
         """
-        return self.create_oval(x - radius, y - radius, x + radius, y + radius, width=0, fill=fill, outline=outline)
+        return self.create_oval(x - radius, y - radius, x + radius, y + radius, width=width, fill=fill, outline=outline)
 
 def main():
     master = Tk()
 
     w = DerivedCanvas(master, width=400, height=400)
     w.pack()
-    
-    text = w.create_text(50,10, text="tk test")
-    circle = w.create_circle(100, 100, 20, None, "black")
+    circle = w.create_circle(100, 100, 20, "white", "black", 1)
 
     mainloop()
 
